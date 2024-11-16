973ee10d2dda56963badf3e88bd59fa4fb9aeec6
Omar-Maalej <omar.maalej@insat.ucar.tn>
2024-11-16 11:24:47 +0100

    async pipe in cv done

--- src/app/cv/cv/cv.component.ts
+++ src/app/cv/cv/cv.component.ts
@@ -3,35 +3,39 @@ import { Cv } from "../model/cv";
 import { LoggerService } from "../../services/logger.service";
 import { ToastrService } from "ngx-toastr";
 import { CvService } from "../services/cv.service";
+import { catchError, of } from "rxjs";
 @Component({
   selector: "app-cv",
   templateUrl: "./cv.component.html",
   styleUrls: ["./cv.component.css"],
 })
 export class CvComponent {
-  cvs: Cv[] = [];
   selectedCv: Cv | null = null;
   /*   selectedCv: Cv | null = null; */
   date = new Date();
+
+  cvs$ = this.cvService.getCvs().pipe(
+    catchError((error) => {
+      this.toastr.error(`
+        Attention!! Les données sont fictives, problème avec le serveur.
+        Veuillez contacter l'admin.`);
+      return of(this.cvService.getFakeCvs());
+    })
+  );
+ 
+
+  selectedCv$ = this.cvService.selectCv$;
 
   constructor(
     private logger: LoggerService,
     private toastr: ToastrService,
     private cvService: CvService
   ) {
-    this.cvService.getCvs().subscribe({
-      next: (cvs) => {
-        this.cvs = cvs;
-      },
-      error: () => {
-        this.cvs = this.cvService.getFakeCvs();
-        this.toastr.error(`
-          Attention!! Les données sont fictives, problème avec le serveur.
-          Veuillez contacter l'admin.`);
-      },
-    });
     this.logger.logger("je suis le cvComponent");
     this.toastr.info("Bienvenu dans notre CvTech");
-    this.cvService.selectCv$.subscribe((cv) => (this.selectedCv = cv));
+  }
+
+  selectCv(cv: Cv) {
+    this.cvService.selectCv(cv);
   }
 }
