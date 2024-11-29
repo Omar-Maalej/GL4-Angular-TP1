9534c10bcee2407db03d927f108fed05c8e51aee
Omar-Maalej <omar.maalej@insat.ucar.tn>
2024-11-29 14:42:01 +0100

    autocomplete

--- src/app/cv/autocomplete/autocomplete.component.ts
+++ src/app/cv/autocomplete/autocomplete.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from "@angular/core";
 import { FormBuilder, AbstractControl } from "@angular/forms";
-import { debounceTime, distinctUntilChanged, switchMap, tap } from "rxjs";
+import { debounceTime, distinctUntilChanged, of, switchMap, tap } from "rxjs";
 import { CvService } from "../services/cv.service";
+import { Cv } from "../model/cv";
 
 @Component({
   selector: "app-autocomplete",
@@ -14,5 +15,38 @@ export class AutocompleteComponent {
   get search(): AbstractControl {
     return this.form.get("search")!;
   }
-  form = this.formBuilder.group({ search: [""] });
+  form = this.formBuilder.group({ search: ["gafsa"] });
+
+  dropdownList : Cv[]= [];
+  loading = false;
+
+  constructor() {
+    this.search.valueChanges
+      .pipe(
+        debounceTime(300), 
+        distinctUntilChanged(), 
+        tap(() => (this.loading = true)),
+        switchMap((value) => {
+          if(value === '') {
+            this.loading = false;
+            return of([]);
+          }else {
+           return  this.cvService.selectByName(value).pipe(
+              tap(() => (this.loading = false))
+            )
+          }
+        }
+        )
+      )
+      .subscribe({
+        next: (results) => {
+          this.dropdownList = results;
+        },
+        error: () => {
+          this.loading = false;
+          this.dropdownList = [];
+        },
+      });
+  }
+
 }
