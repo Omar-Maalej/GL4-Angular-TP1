ab77fd13453b08920ab2810a3484f57d47c360e1
Omar-Maalej <omar.maalej@insat.ucar.tn>
2024-11-29 15:56:25 +0100

    products done

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,6 +10,7 @@ import {
 import { Product } from "./dto/product.dto";
 import { ProductService } from "./services/product.service";
 import { Settings } from "./dto/product-settings.dto";
+import { ProductApiResponse } from "./dto/product-api-response.dto";
 
 @Component({
   selector: "app-products",
@@ -19,6 +20,38 @@ import { Settings } from "./dto/product-settings.dto";
 export class ProductsComponent {
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
-  products$!: Observable<Product[]>;
-  constructor() {}
+  products$: Observable<Product[]>;
+  private loadMoreSubject = new BehaviorSubject<void>(undefined); 
+  private limit = 12; 
+  private skip = 0; 
+  private total = 0; 
+  private hasMoreProducts = true; 
+  private maxSkip = 108;
+
+  constructor(private productService: ProductService) {
+    this.products$ = this.loadMoreSubject.pipe(
+      takeWhile(() => this.skip <= this.maxSkip), 
+      concatMap(() => this.fetchProducts()), 
+      scan((acc : Product[], products) => [...acc, ...products], []), 
+    );
+  }
+
+  private fetchProducts(): Observable<Product[]> {
+    const settings: Settings = { limit: this.limit, skip: this.skip };
+    return this.productService.getProducts(settings).pipe(
+      map((response: ProductApiResponse) => {
+        this.skip += this.limit; 
+        this.total = response.total;
+        this.hasMoreProducts = this.skip <= this.maxSkip; 
+        return response.products; 
+      })
+    );
+  }
+
+  loadMore(): void {
+    if (this.hasMoreProducts) {
+      this.loadMoreSubject.next(); 
+    }
+  }
+
 }
